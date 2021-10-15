612173c9e2b1fb85d7915527c10197844adf61d6
Moody Rahman <moody.byte@gmail.com>
2021-10-15 11:28:50 -0400

    general error handler + test

--- workers/index.js
+++ workers/index.js
@@ -26,18 +26,22 @@ router.post("/post", async request => {
   })
 })
 
-/*
-This is the last route we define, it will match anything that hasn't hit a route we've defined
-above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
+router.get("/error", async request => {
+  throw 55;
+  return new Response("no error");
+})
 
-Visit any page that doesn't exist (e.g. /foobar) to see it in action.
-*/
+const errorHandler = (error) => {
+  console.log(error)
+  return new Response("internal server error", {status:500})
+}
+
+
 router.all("*", () => new Response("404, not found!", { status: 404 }))
-
-/*
-This snippet ties our worker to the router we deifned above, all incoming requests
-are passed to the router where your routes are called and the response is sent.
-*/
 addEventListener('fetch', (e) => {
-  e.respondWith(router.handle(e.request))
+  e.respondWith(
+    router
+    .handle(e.request)
+    .catch(errorHandler)
+    )
 })
