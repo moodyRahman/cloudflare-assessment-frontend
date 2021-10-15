0ff66d560c3ac5be24e4a3bb8adaa5bf1c3cf9ef
Moody Rahman <moody.byte@gmail.com>
2021-10-15 11:28:35 -0400

    deleted unneeded template code

--- workers/index.js
+++ workers/index.js
@@ -10,46 +10,7 @@ router.get("/", () => {
   return new Response("Hello, world! This is the root page of your Worker template.")
 })
 
-/*
-This route demonstrates path parameters, allowing you to extract fragments from the request
-URL.
-
-Try visit /example/hello and see the response.
-*/
-router.get("/example/:text", ({ params }) => {
-  // Decode text like "Hello%20world" into "Hello world"
-  let input = decodeURIComponent(params.text)
-
-  // Construct a buffer from our input
-  let buffer = Buffer.from(input, "utf8")
-
-  // Serialise the buffer into a base64 string
-  let base64 = buffer.toString("base64")
-
-  // Return the HTML with the string to the client
-  return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
-    headers: {
-      "Content-Type": "text/html"
-    }
-  })
-})
-
-/*
-This shows a different HTTP method, a POST.
-
-Try send a POST request using curl or another tool.
-
-Try the below curl command to send JSON:
-
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
-*/
 router.post("/post", async request => {
-  // Create a base object with some fields.
-  let fields = {
-    "asn": request.cf.asn,
-    "colo": request.cf.colo
-  }
-
   // If the POST data is JSON then attach it to our response.
   if (request.headers.get("Content-Type") === "application/json") {
     fields["json"] = await request.json()
