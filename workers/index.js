d4746b93059f883b5ede1a0ce7a5ac5f0c70550f
Moody Rahman <moody.byte@gmail.com>
2021-10-15 20:36:01 -0400

    prettified template code

--- workers/index.js
+++ workers/index.js
@@ -1,26 +1,31 @@
-import { Router } from 'itty-router'
+import { Router } from "itty-router";
 
 // Create a new router
-const router = Router()
+const router = Router();
 
 /*
 Our index route, a simple hello world.
 */
 router.get("/", () => {
-  return new Response("Hello, world! This is the root page of your Worker template.")
-})
+  return new Response(
+    "Hello, world! This is the root page of your Worker template."
+  );
+});
 
-router.post("/post", async request => {
+router.post("/post", async (request) => {
   // If the POST data is JSON then attach it to our response.
-  if (request.headers.get("Content-Type") === "application/json") {
-    fields["json"] = await request.json()
-  }
+  const data = await request.json();
+
+  await POSTS_DATABASE.put(data.key, data.value);
 
   // Serialise the JSON to a string.
-  const returnData = JSON.stringify(fields, null, 2);
 
-  return new Response(returnData, {
+  return new Response(`inserted ${data.key}`, {
     headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
 
 router.get("/post", async (request) => {
   const allPosts = await POSTS_DATABASE.list();
@@ -34,22 +39,21 @@ router.get("/post", async (request) => {
   return new Response(out)
 });
 
-router.get("/error", async request => {
+router.get("/error", async (request) => {
   throw 55;
   return new Response("no error");
-})
+});
 
 const errorHandler = (error) => {
-  console.log(error)
-  return new Response("internal server error", {status:500})
-}
+  console.log(error.stack);
+  console.log(error.message)
+  return new Response("internal server error", { status: 500 });
+};
 
-
-router.all("*", () => new Response("404, not found!", { status: 404 }))
-addEventListener('fetch', (e) => {
+router.all("*", () => new Response("404, not found!", { status: 404 }));
+addEventListener("fetch", (e) => {
   e.respondWith(
     router
     .handle(e.request)
-    .catch(errorHandler)
-    )
-})
+    .catch(errorHandler));
+});
