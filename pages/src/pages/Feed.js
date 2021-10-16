ab7d80b294fee724a3761ad517a25c0c859a96bf
Moody Rahman <moody.byte@gmail.com>
2021-10-16 16:58:19 -0400

    get all posts and render their raw JSON

--- pages/src/pages/Feed.js
+++ pages/src/pages/Feed.js
@@ -1,14 +1,42 @@
-import { Container } from 'react-bootstrap';
-
+import { useContext, useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { useHistory } from "react-router";
+import { AccessTokenContext } from "../context/LoginContext";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+	const token = useContext(AccessTokenContext).accessToken;
+  let history = useHistory();
+	useEffect(() => {
+		if (token === "")
+		{
+			history.push("/")
+			return
+		}
 
-    return (
+    fetch("https://workers.moodyrahman.workers.dev/post", { mode: "cors" })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+        console.log(data);
+      });
+  });
 
-        <Container>
-            hello this is the feed
-        </Container>
-    )
-}
+  if (loading) {
+    return <Container>loading screen</Container>;
+  }
+
+  return (
+    <Container>
+      {posts.map((e) => (
+        <div>{JSON.stringify(e)}</div>
+      ))}
+    </Container>
+  );
+};
 
 export default Feed;
