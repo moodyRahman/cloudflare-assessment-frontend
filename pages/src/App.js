e03242700021c198e4fe61e1521602b6c98c64d0
Moody Rahman <moody.byte@gmail.com>
2021-10-16 12:40:03 -0400

    react app bootstrap template

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            ABOUT
+          </Route>
+          <Route path="/users">
+            USSERS
+          </Route>
+          <Route path="/">
+            HOME
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
